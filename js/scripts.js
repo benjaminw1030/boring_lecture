eb05bc3e9573ac51592641cbe70ffab67f4d6be5
Ben Wilson <benjaminw1030@gmail.com>
2021-08-10 13:38:33 -0700

    Add hightlighting functionality to paragraphs
    Co-authored-by: Anthony McRae <toddmac12@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $("#p1").click(function() {
+    $(".paragraph").removeClass("highlight");
+    $("#p1").addClass("highlight");
+  });
+
+  $("#p2").click(function() {
+    $(".paragraph").removeClass("highlight");
+    $("#p2").addClass("highlight");
+  });
+
+  $("#p3").click(function() {
+    $(".paragraph").removeClass("highlight");
+    $("#p3").addClass("highlight");
+  });
+  
+  $("#p4").click(function() {
+    $(".paragraph").removeClass("highlight");
+    $("#p4").addClass("highlight");
+  });
+
+  $("#p5").click(function() {
+    $(".paragraph").removeClass("highlight");
+    $("#p5").addClass("highlight");
+  });
+});
